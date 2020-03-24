39ae84e01a19fb540ae68f9a1ec06cd5c503b828
mingy98 <ming_ying@college.harvard.edu>
2020-03-24 14:40:02 -0400

    latest master

--- src/components/PageAuth/Auth.js
+++ src/components/PageAuth/Auth.js
@@ -31,7 +31,6 @@ class Auth extends Component {
   }
 
   handleLogin = (evt) => {
-    console.log("LOG IN HANDLE")
     this.setState({processing: true, failed_login_attempt: false})
     this.props.dispatch(userLogin(this.state.emailLogin, this.state.passwordLogin, false));
   }
