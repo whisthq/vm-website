19ba0e48f0e12dd4f15ec4c5a2efc5a2f6d0fd6e
mingy98 <ming@fractalcomputers.com>
2020-06-26 09:59:43 -0400

    cehck verified email

--- src/store/sagas/auth/signup_saga.js
+++ src/store/sagas/auth/signup_saga.js
@@ -28,7 +28,7 @@ function* userSignup(action) {
             );
             yield put(LoginAction.loginSuccess());
             yield put(TokenAction.storeVerificationToken(json.token));
-            yield call(checkVerifiedEmail, action);
+            yield put(SignupAction.checkVerifiedEmail(action.user));
             yield put(CustomerAction.getPromoCode(action.user, json.token));
             yield put(
                 SignupAction.sendVerificationEmail(action.user, json.token)
@@ -41,6 +41,7 @@ function* userSignup(action) {
 }
 
 function* checkVerifiedEmail(action) {
+    console.log(action);
     yield select();
     const { json } = yield call(
         apiPost,
