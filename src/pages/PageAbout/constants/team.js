e8def598ef505ab5e3c28cfe128e557a1b0adc3c
Jonathan <jonathanxu25@gmail.com>
2020-07-14 11:17:30 -0400

    shuffled team section

--- src/pages/PageAbout/constants/team.js
+++ src/pages/PageAbout/constants/team.js
@@ -1,47 +1,56 @@
-export const team = [
+export const teamData = [
     {
-        image="assets/team_photos/ming.svg",
-        name="Ming Ying",
-        text="Ming was a software engineer at Fidelity and quant researcher at AQR Capital Management. His previous research interests include linear optimization algorithms in the context of daily fantasy baseball betting and Bayesian modelling. He studied Statistics and Computer Science at Harvard."
+        image: "ming",
+        name: "Ming Ying",
+        text:
+            "Ming was a software engineer at Fidelity and quant researcher at AQR Capital Management. His previous research interests include linear optimization algorithms in the context of daily fantasy baseball betting and Bayesian modelling. He studied Statistics and Computer Science at Harvard.",
     },
     {
-        image="assets/team_photos/phil.svg",
-        name="Phil Noel",
-        text="Prior to Phil was a product manager at Microsoft Azure, machine learning researcher at Premier Tech, and speaker at TedX Harvard and TedX Quebec. Phil graduated from Harvard University, where he studied Computer Science and Neuroscience."
+        image: "phil",
+        name: "Phil Noel",
+        text:
+            "Prior to Phil was a product manager at Microsoft Azure, machine learning researcher at Premier Tech, and speaker at TedX Harvard and TedX Quebec. Phil graduated from Harvard University, where he studied Computer Science and Neuroscience.",
     },
     {
-        image="assets/team_photos/nick.svg",
-        name="Nick Pipitone",
-        text="Nick has an affinity for graphics shaders, circuits, and cryptocurrencies, with particular experience in cybersecurity and penetration testing. He studied Information Systems at Carnegie Mellon."
+        image: "nick",
+        name: "Nick Pipitone",
+        text:
+            "Nick has an affinity for graphics shaders, circuits, and cryptocurrencies, with particular experience in cybersecurity and penetration testing. He studied Information Systems at Carnegie Mellon.",
     },
     {
-        image="assets/team_photos/roshan.svg",
-        name="Roshan Padaki",
-        text="Prior to Fractal, Roshan researched performance lower bounds for matching algorithms in dynamic matching markets and developed methods for reducing dimensionality and sampling parameters in Bayesian deep learning. He studied mathematics at Harvard."
+        image: "roshan",
+        name: "Roshan Padaki",
+        text:
+            "Prior to Fractal, Roshan researched performance lower bounds for matching algorithms in dynamic matching markets and developed methods for reducing dimensionality and sampling parameters in Bayesian deep learning. He studied mathematics at Harvard.",
     },
     {
-        image="assets/team_photos/hamish.svg",
-        name="Hamish Nicholson",
-        text="Hamish has worked as a data scientist at NASA's Jet Propulsion Laboratory, as a deep learning researcher at Perceptive Automata, and as a research partner at the Radcliffe Institute, where he studied the formation of molecular clouds in young stars. He studied Computer Science and Statistics at Harvard University."
+        image: "hamish",
+        name: "Hamish Nicholson",
+        text:
+            "Hamish has worked as a data scientist at NASA's Jet Propulsion Laboratory, as a deep learning researcher at Perceptive Automata, and as a research partner at the Radcliffe Institute, where he studied the formation of molecular clouds in young stars. He studied Computer Science and Statistics at Harvard University.",
     },
     {
-        image="assets/team_photos/matt.svg",
-        name="Mathieu Lochet",
-        text="Mathieu studied Computer Science and engineering at INSA in France. He has worked in software architecture, framework design, optimization and game engines for the last 10 years."
+        image: "matt",
+        name: "Mathieu Lochet",
+        text:
+            "Mathieu studied Computer Science and engineering at INSA in France. He has worked in software architecture, framework design, optimization and game engines for the last 10 years.",
     },
     {
-        image="assets/team_photos/jonathan.svg",
-        name="Jonathan Xu",
-        text="Jonathan is an avid hackathon-goer, and loves to build creative projects from the ground up. Prior to Fractal, he did full stack development for startups. He is studying software engineering at the University of Waterloo."
+        image: "jonathan",
+        name: "Jonathan Xu",
+        text:
+            "Jonathan is an avid hackathon-goer, and loves to build creative projects from the ground up. Prior to Fractal, he did full stack development for startups. He is studying software engineering at the University of Waterloo.",
     },
     {
-        image="assets/team_photos/isabelle.svg",
-        name="Isabelle Zheng",
-        text="Isabelle studies Computer Science at Harvard University. Prior to working with Fractal, she did mobile and full-stack development with Facebook and other startups. On campus, she works to connect student founders with potential investors."
+        image: "isabelle",
+        name: "Isabelle Zheng",
+        text:
+            "Isabelle studies Computer Science at Harvard University. Prior to working with Fractal, she did mobile and full-stack development with Facebook and other startups. On campus, she works to connect student founders with potential investors.",
     },
     {
-        image="assets/team_photos/sean.svg",
-        name="Sean Hackett",
-        text="Sean has milled around quantum physics labs, hackathons, and nonprofits. He studied tiny trampolines that transmute quantum information and built robots that make sense of the 'hmms' and 'uh huhs' of everyday conversation. Sean studied Computer Science and Physics at Yale, and will enroll in the University of Southern California for a Ph.D in Computer Science."
-    }
-]
+        image: "sean",
+        name: "Sean Hackett",
+        text:
+            "Sean has milled around quantum physics labs, hackathons, and nonprofits. He studied tiny trampolines that transmute quantum information and built robots that make sense of the 'hmms' and 'uh huhs' of everyday conversation. Sean studied Computer Science and Physics at Yale, and will enroll in the University of Southern California for a Ph.D in Computer Science.",
+    },
+];
