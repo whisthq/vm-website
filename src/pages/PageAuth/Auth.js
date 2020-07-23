3739e76103f2c82e95f4313d91fe98a508088f63
mingy98 <ming@fractalcomputers.com>
2020-07-23 16:38:12 -0400

    working latest

--- src/pages/PageAuth/Auth.js
+++ src/pages/PageAuth/Auth.js
@@ -21,7 +21,7 @@ class Auth extends Component {
             modalShow: false,
             showPopup: false,
             processing: false,
-            google_button_active: true,
+            google_button_active: false,
         };
         this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     }
@@ -54,121 +54,121 @@ class Auth extends Component {
                 {this.props.loggedIn && this.props.email_verified ? (
                     <Redirect to="/dashboard" />
                 ) : (
-                    <div
-                        style={{
-                            position: "relative",
-                            margin: "auto",
-                            maxWidth: 1920,
-                        }}
-                    >
-                        <Header color="#333333" button="#5ec3eb" homepage />
                         <div
                             style={{
-                                minHeight: "100vh",
-                                paddingTop: 50,
-                                backgroundColor: "white",
                                 position: "relative",
+                                margin: "auto",
+                                maxWidth: 1920,
                             }}
                         >
+                            <Header color="#333333" button="#5ec3eb" homepage />
                             <div
                                 style={{
-                                    backgroundColor: "rgba(0,0,0,0.0)",
-                                    borderRadius: 2,
-                                    border: "solid 1px white",
-                                    padding: "20px 40px 60px 40px",
-                                    marginBottom: 80,
-                                    margin: "auto",
+                                    minHeight: "100vh",
+                                    paddingTop: 50,
+                                    backgroundColor: "white",
+                                    position: "relative",
                                 }}
                             >
-                                <Tabs>
-                                    <TabList
-                                        style={{
-                                            textAlign: "center",
-                                            border: "none",
-                                            fontWeight: "bold",
-                                            fontSize: 16,
-                                        }}
-                                    >
-                                        <Tab
-                                            onClick={() =>
-                                                this.setState({
-                                                    failed_signup_attempt: false,
-                                                })
-                                            }
-                                        >
-                                            LOG IN
-                                        </Tab>
-                                        <Tab
-                                            onClick={() =>
-                                                this.setState({
-                                                    failed_login_attempt: false,
-                                                })
-                                            }
-                                        >
-                                            SIGN UP
-                                        </Tab>
-                                    </TabList>
-                                    <TabPanel style={{ padding: "15px 30px" }}>
-                                        {this.props.google_auth &&
-                                            this.props.google_auth
-                                                .show_google_button &&
-                                            this.state.google_button_active && (
-                                                <GoogleBox
-                                                    processing={
-                                                        this.state.processing
-                                                    }
-                                                    setProcessing={
-                                                        this.setProcessing
-                                                    }
-                                                />
-                                            )}
-                                        <LoginBox
-                                            processing={this.state.processing}
-                                            setProcessing={this.setProcessing}
-                                        />
-                                        <HashLink
-                                            to="/reset"
+                                <div
+                                    style={{
+                                        backgroundColor: "rgba(0,0,0,0.0)",
+                                        borderRadius: 2,
+                                        border: "solid 1px white",
+                                        padding: "20px 40px 60px 40px",
+                                        marginBottom: 80,
+                                        margin: "auto",
+                                    }}
+                                >
+                                    <Tabs>
+                                        <TabList
                                             style={{
-                                                textDecoration: "none",
+                                                textAlign: "center",
+                                                border: "none",
+                                                fontWeight: "bold",
+                                                fontSize: 16,
                                             }}
                                         >
-                                            <div
+                                            <Tab
+                                                onClick={() =>
+                                                    this.setState({
+                                                        failed_signup_attempt: false,
+                                                    })
+                                                }
+                                            >
+                                                LOG IN
+                                        </Tab>
+                                            <Tab
+                                                onClick={() =>
+                                                    this.setState({
+                                                        failed_login_attempt: false,
+                                                    })
+                                                }
+                                            >
+                                                SIGN UP
+                                        </Tab>
+                                        </TabList>
+                                        <TabPanel style={{ padding: "15px 30px" }}>
+                                            {this.props.google_auth &&
+                                                this.props.google_auth
+                                                    .show_google_button &&
+                                                this.state.google_button_active && (
+                                                    <GoogleBox
+                                                        processing={
+                                                            this.state.processing
+                                                        }
+                                                        setProcessing={
+                                                            this.setProcessing
+                                                        }
+                                                    />
+                                                )}
+                                            <LoginBox
+                                                processing={this.state.processing}
+                                                setProcessing={this.setProcessing}
+                                            />
+                                            <HashLink
+                                                to="/reset"
                                                 style={{
-                                                    textAlign: "center",
-                                                    marginTop: 25,
-                                                    color: "#333333",
                                                     textDecoration: "none",
-                                                    fontSize: 13,
                                                 }}
                                             >
-                                                Forgot Password?
+                                                <div
+                                                    style={{
+                                                        textAlign: "center",
+                                                        marginTop: 25,
+                                                        color: "#333333",
+                                                        textDecoration: "none",
+                                                        fontSize: 13,
+                                                    }}
+                                                >
+                                                    Forgot Password?
                                             </div>
-                                        </HashLink>
-                                    </TabPanel>
-                                    <TabPanel style={{ padding: "15px 30px" }}>
-                                        {this.props.google_auth &&
-                                            this.props.google_auth
-                                                .show_google_button &&
-                                            this.state.google_button_active && (
-                                                <GoogleBox
-                                                    processing={
-                                                        this.state.processing
-                                                    }
-                                                    setProcessing={
-                                                        this.setProcessing
-                                                    }
-                                                />
-                                            )}
-                                        <SignupBox
-                                            processing={this.state.processing}
-                                            setProcessing={this.setProcessing}
-                                        />
-                                    </TabPanel>
-                                </Tabs>
+                                            </HashLink>
+                                        </TabPanel>
+                                        <TabPanel style={{ padding: "15px 30px" }}>
+                                            {this.props.google_auth &&
+                                                this.props.google_auth
+                                                    .show_google_button &&
+                                                this.state.google_button_active && (
+                                                    <GoogleBox
+                                                        processing={
+                                                            this.state.processing
+                                                        }
+                                                        setProcessing={
+                                                            this.setProcessing
+                                                        }
+                                                    />
+                                                )}
+                                            <SignupBox
+                                                processing={this.state.processing}
+                                                setProcessing={this.setProcessing}
+                                            />
+                                        </TabPanel>
+                                    </Tabs>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )}
+                    )}
             </div>
         );
     }
@@ -181,11 +181,11 @@ function mapStateToProps(state) {
         google_auth: state.AuthReducer.google_auth
             ? state.AuthReducer.google_auth
             : {
-                  needs_reason: false,
-                  use_google: false,
-                  auth_error: "",
-                  show_google_button: true,
-              },
+                needs_reason: false,
+                use_google: false,
+                auth_error: "",
+                show_google_button: true,
+            },
     };
 }
 
