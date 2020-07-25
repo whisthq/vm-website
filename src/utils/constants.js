a4d6448820bd42e9faeb73ebc83ff5dd3c2bce06
mingy98 <ming@fractalcomputers.com>
2020-07-25 00:05:37 -0400

    local

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const production = {
     url: {
-        PRIMARY_SERVER: "https://cube-celery-vm.herokuapp.com",
+        PRIMARY_SERVER: "https://main-webserver.fractalcomputers.com",
     },
     stripe: {
         PUBLIC_KEY: "pk_live_XLjiiZB93KN0EjY8hwCxvKmB00whKEIj3U",
@@ -9,7 +9,7 @@ const production = {
     azure: {
         RESOURCE_GROUP: "Fractal",
     },
-    new_server: false,
+    new_server: true,
 };
 
 const staging = {
@@ -53,7 +53,7 @@ const local = {
 
 // TODO: Change config to development/production after changes in staging are deployed to the other vm webservers
 export const config =
-    process.env.NODE_ENV === "development" ? development : staging;
+    process.env.NODE_ENV === "development" ? staging : staging;
 
 export const GOOGLE_CLIENT_ID =
     "581514545734-7k820154jdfp0ov2ifk4ju3vodg0oec2.apps.googleusercontent.com";
