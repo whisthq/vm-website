fb0f12c583810a76767fe3d40a28759c96ebc251
Philippe Noël <phil@fractalcomputers.com>
2020-08-05 08:13:10 -0400

    typo

--- src/pages/PageChangelog/constants/changes.js
+++ src/pages/PageChangelog/constants/changes.js
@@ -15,7 +15,7 @@ export const changelogData = [
             {
                 type: "applications",
                 msg:
-                    "We have added a 'How to use' section to the applications with information on how to make the most of all the features available with Fractal.",
+                    "We have added a 'How to use' section to the applications, with information on how to make the most of all the features available with Fractal.",
             },
         ],
     },
@@ -25,12 +25,12 @@ export const changelogData = [
             {
                 type: "streaming",
                 msg:
-                    "We've fixed a video decoding bug which caused infrequent crashing on certain older devices, and have added support fro H.265 video compression.",
+                    "We've fixed a video decoding bug which caused infrequent crashing on certain older devices, and have added support for H.265 video compression.",
             },
             {
                 type: "website",
                 msg:
-                    "We have added application pre-installing on Windows cloud PCs. You can now select up to 40+ applications to be preinstalled for you.",
+                    "We have added application pre-installation on Windows cloud PCs. You can now select up to 40+ applications to be preinstalled for you.",
             },
             {
                 type: "applications",
@@ -45,12 +45,12 @@ export const changelogData = [
             {
                 type: "streaming",
                 msg:
-                    "We've added GPU video capture to our Linux streaming protocol and are releasing to beta users..",
+                    "We've added GPU video capture to our Linux streaming and are releasing to beta users.",
             },
             {
                 type: "website",
                 msg:
-                    "Bug fixes and internal back-end changes. Improved AES security.",
+                    "Bug fixes and internal back-end changes. Enhanced AES security.",
             },
         ],
     },
